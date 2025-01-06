726277b54b8378549b7fb15aa29060ef9a95c299
zainvice <amazwings@gmail.com>
2025-01-06 11:02:30 -0700

    v1.0: Fully Working

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { authenticate, isAdmin } = require("../middlewares/authMiddleware");
-const UserController = require("../controllers/UserController");
+const UserController = require("../controllers/userController");
 
 router.post("/register", UserController.register);
 
